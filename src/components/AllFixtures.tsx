6365373d4afbe2a2a8ba4d342e0d23ad50823f10
oli giles <ohg3@hotmail.co.uk>
2020-06-19 09:23:47 +0100

    first commit / imgs

---
+++ src/components/AllFixtures.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const AllFixtures: React.FC = () => {
+
+  const key: string = "";
+  
+  const allFixturesAPI = async () =>{
+    const response = fetch("");
+    const data = await response;
+  }
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
